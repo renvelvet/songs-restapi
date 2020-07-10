280c3a67b3f94867c96582923854a9f94eb55152
Resha Puspita <reshapuspita734@gmail.com>
2020-07-10 17:58:12 +0700

    add files

---
+++ album/songs.js
@@ -0,0 +1,17 @@
+let songs = [
+  { id: 0, title: "Cry Little Heart", album: "Constellation" },
+  { id: 1, title: "The Magician", album: "Rainbow Ail" },
+  { id: 2, title: "Man Upon the Hill", album: "Constellation" },
+  { id: 3, title: "Frozen Scratch Cerulean", album: "SRxBS" },
+  { id: 4, title: "Solace", album: "SRxBS" },
+  { id: 5, title: "Prelude", album: "SRxBS" },
+  { id: 6, title: "Any Day in the Park", album: "Rainbow Ail" },
+  { id: 7, title: "Woth It", album: "Constellation" },
+  { id: 8, title: "Little Mischievous", album: "Rainbow Ail" },
+  { id: 9, title: "St. Ann", album: "Rainbow Ail" },
+  { id: 10, title: "Blue Boat Lovers", album: "Rainbow Ail" },
+  { id: 11, title: "Story of Them All", album: "Rainbow Ail" },
+  { id: 12, title: "In the Meantime", album: "Rainbow Ail" },
+];
+
+module.exports = songs;
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+const songs = require("./album/songs");
+
+app.use(express.json()); // body-parser
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
+});
+
+app.get("/songs", (req, res) => {
+  res.send(songs);
+});
+
+app.post("/songs", (req, res) => {
+  const { title, album } = req.body;
+  const id = songs[songs.length - 1].id + 1;
+  songs.push({ id, title, album });
+  console.log(id);
+
+  res.send({
+    message: "Data berhasil ditambahkan",
+    songAdded: songs,
+  });
+});
+
+app.put("/song/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, album } = req.body;
+
+  let newTitle;
+  let newAlbum;
+
+  if (title === null || title === "") {
+    newTitle = songs[id].title;
+  } else {
+    newTitle = title;
+  }
+
+  if (album === null || album === "") {
+    newAlbum = songs[id].album;
+  } else {
+    newAlbum = album;
+  }
+
+  songs.splice(id, 1, {
+    id,
+    title: newTitle,
+    album: newAlbum,
+  });
+
+  res.send({
+    message: "Data berhasil diubah",
+    updatedSongs: songs,
+  });
+});
+
+app.delete("/song/:id", (req, res) => {
+  const id = req.params.id;
+
+  songs.splice(id, 1);
+
+  res.send({
+    message: "Data berhasil dihapus",
+    updatedSongs: songs,
+  });
+});
+
+app.listen(PORT, () => console.log(`Server run at PORT ${PORT}`));
